5fbe62db02e72b75c17576309fe282ae2ca14bc9
AndreasPlangger <andreas_plangger@hotmail.com>
2022-02-18 23:24:38 +0100

    add images and array

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { MenuComponent } from './menu/menu.component';
+import { CartComponent } from './cart/cart.component';
+import { DetailsComponent } from './details/details.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: HomeComponent
+}, {
+  path: 'about-us',
+  component: AboutUsComponent
+}, {
+  path: 'menu',
+  component: MenuComponent
+}, {
+  path: 'cart',
+  component: CartComponent
+}, {
+  path: 'details',
+  component: DetailsComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -8,6 +8,74 @@ import { Component, OnInit } from '@angular/core';
 export class MenuComponent implements OnInit {
 
   // name, image, description, price and availability (Boolean).
+  products: Array<{ name: string, img: any, description: string, price: number, availability: boolean, }> = [
+
+    {
+      name: 'Pizza Provencale',
+      img: '../../assets/img/pizza_provencale.jpg',
+      description: 'fg',
+      price: 22,
+      availability: true,
+
+    },
+    {
+      name: 'Pizza Vegetariana',
+      img: '../../assets/img/pizza_vegetariana_alt.jpg',
+      description: '',
+      price: 22,
+      availability: true,
+    },
+    {
+      name: 'Pizza Picante',
+      img: '../../assets/img/pizza_picante.jpg',
+      description: '',
+      price: 22,
+      availability: true,
+    },
+    {
+      name: 'Pizza Salame',
+      img: '../../assets/img/pizza_salame.jpg',
+      description: '',
+      price: 22,
+      availability: true,
+    },
+    {
+      name: 'Spaghetti di Mare',
+      img: '../../assets/img/spaghetti_di_mare.jpg',
+      description: '',
+      price: 22,
+      availability: true,
+
+    },
+    {
+      name: 'Lasagna',
+      img: '../../assets/img/lasagna.jpg',
+      description: '',
+      price: 22,
+      availability: true,
+    },
+    {
+      name: 'Insalata Mista',
+      img: '../../assets/img/insalata_mista.jpg',
+      description: '',
+      price: 22,
+      availability: true,
+    },
+    {
+      name: 'Dulce de Leche',
+      img: '../../assets/img/dulce_de_leche.jpg',
+      description: '',
+      price: 22,
+      availability: true,
+    },
+    {
+      name: 'Panna Cotta',
+      img: '../../assets/img/panna_cotta.jpg',
+      description: '',
+      price: 22,
+      availability: true,
+    }
+  ]
 
   constructor() { }
 
