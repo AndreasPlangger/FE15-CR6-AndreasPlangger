7b029dff17be55a6d065d28d00abfc844b23a7f9
AndreasPlangger <andreas_plangger@hotmail.com>
2022-02-18 09:42:28 +0100

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { MenuComponent } from './menu/menu.component';
+import { CartComponent } from './cart/cart.component';
+import { DetailsComponent } from './details/details.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    AboutUsComponent,
+    MenuComponent,
+    CartComponent,
+    DetailsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
