5e0e88225972aa2aa1def84cdeac022a5165fd37
AndreasPlangger <andreas_plangger@hotmail.com>
2022-02-21 22:42:50 +0100

    add details function

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-
+import { CartService } from '../cart.service';
 import { products } from '../products';
 import { IProducts } from '../IProducts';
 
@@ -13,9 +13,17 @@ export class DetailsComponent implements OnInit {
 
   product: IProducts = {} as IProducts;
   id: number = 0;
-  route: any;
 
-  constructor() { }
+
+  constructor(private route: ActivatedRoute,
+    private cartService: CartService) {
+
+  }
+  addToCart() {
+    window.alert('Your product has been added to the cart!');
+    this.cartService.addToCart(this.product);
+  }
+
 
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => {
