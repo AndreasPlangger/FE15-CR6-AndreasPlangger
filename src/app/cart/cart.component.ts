a30740d43db90e6631e9cc425f7f51d279c7e81c
AndreasPlangger <andreas_plangger@hotmail.com>
2022-02-22 22:41:23 +0100

    bugfix

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { CartService } from '../cart.service';
+import { IProducts } from '../IProducts';
 
 @Component({
   selector: 'app-cart',
@@ -6,10 +9,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.scss']
 })
 export class CartComponent implements OnInit {
-
-  constructor() { }
-
+  contact = new FormGroup({
+    fName: new FormControl("", [Validators.required, Validators.minLength(2)]),
+    phone: new FormControl("", [Validators.required, Validators.minLength(2)]),
+    textArea: new FormControl("", [Validators.required, Validators.minLength(2)])
+  })
+  items: IProducts[] = [];
+  constructor(private cartService: CartService) { }
+  get itemPrice(): number {
+    let total: number = 0;
+    for (const item of this.items) {
+      total += item.price;
+    }
+    return total;
+  }
+  get serviceFee(): number {
+    return this.itemPrice * 0.1;
+  }
+  get discount(): number {
+    let total: number = this.itemPrice + this.serviceFee;
+    let discount: number = 0;
+    if (total >= 40) {
+      discount = total * 0.15;
+    }
+    return discount;
+  }
+  get totalPrice(): number {
+    return (this.itemPrice + this.serviceFee);
+  }
   ngOnInit(): void {
+    this.items = this.cartService.getItems();
+  }
+  submitForm() {
+    if (this.contact.valid) {
+      var a = this.contact.value;
+      console.log(a);
+    }
   }
 
 }
